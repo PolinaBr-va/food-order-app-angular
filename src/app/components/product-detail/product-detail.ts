f79ae1e8632f52d2048a11b3870935f6df1644e5
Polina Bryleva <polina.bryleva20@gmail.com>
2025-10-19 18:29:40 +0300

    updated product-detail

--- src/app/components/product-detail/product-detail.ts
+++ src/app/components/product-detail/product-detail.ts
@@ -2,11 +2,11 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product';
 import { CartService } from '../../services/cart';
-import { NgIf, NgFor } from "@angular/common";
+import { NgIf, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FavoriteService } from '../../services/favorite';
 import { Subscription } from 'rxjs';
-import { ActivatedRoute, RouterLink } from "@angular/router";
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ImageService } from '../../services/image';
 import { CommonModule } from '@angular/common';
 
@@ -14,36 +14,35 @@ import { CommonModule } from '@angular/common';
   selector: 'app-product-detail',
   imports: [NgIf, NgFor, FormsModule, RouterLink, CommonModule],
   templateUrl: './product-detail.html',
-  styleUrl: './product-detail.css'
+  styleUrl: './product-detail.css',
 })
 export class ProductDetailComponent implements OnInit, OnDestroy {
-   product: Product | null = null;
-   selectedSize: string = '';
-   quantity: number = 1;
-   isLoading: boolean = true;
-   isFavorite: boolean = false;
+  product: Product | null = null;
+  selectedSize: string = '';
+  quantity: number = 1;
+  isLoading: boolean = true;
+  isFavorite: boolean = false;
 
-   private subscription: Subscription = new Subscription;
+  private subscription: Subscription = new Subscription();
 
-   constructor(
+  constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private cartService: CartService,
     private favoriteService: FavoriteService,
     public imageService: ImageService
-   ) {}
+  ) {}
 
-   ngOnInit() {
-     const productId = Number(this.route.snapshot.paramMap.get('id'));
-     
-     if (productId) {
+  ngOnInit(): void {
+    const productId = Number(this.route.snapshot.paramMap.get('id'));
+
+    if (productId) {
       const productSub = this.productService.getProductsById(productId).subscribe({
         next: (product) => {
           if (product) {
             this.product = product;
             this.isFavorite = this.favoriteService.isFavorite(product.id);
 
-            // Устанавливаем размер по умолчанию
             if (product.sizes && product.sizes.length) {
               this.selectedSize = product.sizes[0];
             }
@@ -52,23 +51,23 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
         },
         error: () => {
           this.isLoading = false;
-        }
+        },
       });
 
       this.subscription.add(productSub);
-     } else {
+    } else {
       this.isLoading = false;
-     }
-   }
+    }
+  }
 
-   onAddToCart() {
+  onAddToCart(): void {
     if (this.product) {
       this.cartService.addToCart(this.product, this.quantity, this.selectedSize);
-      alert('Добавлено в корзину!')
+      alert('Добавлено в корзину!');
     }
-   }
+  }
 
-   toggleFavorite() {
+  toggleFavorite(): void {
     if (this.product) {
       if (this.isFavorite) {
         this.favoriteService.removeFromFavorites(this.product.id);
@@ -76,20 +75,20 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
         this.favoriteService.addToFavorites(this.product);
       }
       this.isFavorite = !this.isFavorite;
-    } 
-   }
+    }
+  }
 
-   increaseQuantity() {
+  increaseQuantity(): void {
     this.quantity++;
-   }
+  }
 
-   decreaseQuantity() {
+  decreaseQuantity(): void {
     if (this.quantity > 1) {
       this.quantity--;
     }
-   }
+  }
 
-   ngOnDestroy() {
+  ngOnDestroy(): void {
     this.subscription.unsubscribe();
-   }
+  }
 }
