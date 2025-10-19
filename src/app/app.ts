57eb90a6512211abef8ed0de12b0700ab30d10c4
Polina Bryleva <polina.bryleva20@gmail.com>
2025-10-19 18:48:12 +0300

    updated app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet, RouterModule, Router, NavigationEnd} from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterOutlet, RouterModule } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
-import { CartService } from './services/cart';
-import { filter } from 'rxjs';
-import { OrderSuccessModalComponent } from "./components/order-success-modal/order-success-modal";
+import { OrderSuccessModalComponent } from './components/order-success-modal/order-success-modal';
+import { ProgressService } from './services/progress';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, RouterModule, CommonModule, NgIf, OrderSuccessModalComponent],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    RouterModule,
+    CommonModule,
+    NgIf,
+    OrderSuccessModalComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
   currentStep = 1;
@@ -17,40 +24,18 @@ export class App implements OnInit {
   showOrderSuccesModal = false;
   cartItemCount = 0;
 
-  constructor(private router: Router, private cartService: CartService) {}
+  constructor(private progressService: ProgressService) {}
 
   ngOnInit() {
-    // Отслеживаем изменения маршрута для обновления прогресс-бара
-    this.router.events
-     .pipe(filter(event => event instanceof NavigationEnd))
-     .subscribe((event: NavigationEnd) => {
-       this.updateProgressBar(event.url);
-     });
-
-     // Подписываемся на изменения в корзине
-     this.cartService.getCartItems().subscribe(items => {
-      this.cartItemCount = items.reduce((total, item) => total + item.quantity, 0);
-     });
-  
-     // Подписываемся на события успешного заказа
-     this.cartService.orderSuccess$.subscribe(success => {
-      if (success) {
-        this.showOrderSuccesModal = true;
-        this.currentStep = 3;
-      }
-     });
-    }
-
-  updateProgressBar(url: string) {
-    if (url.includes('/home') || url === '/') {
-      this.currentStep = 1;
-      this.showProgressBar = true;
-    } else if (url.includes('/cart')) {
-      this.currentStep = 2;
-      this.showProgressBar = true;
-    } else {
-      this.showProgressBar = false;
-    }
-  }
+    this.progressService.currentStep$.subscribe((step) => (this.currentStep = step));
+    this.progressService.showProgressBar$.subscribe((show) => (this.showProgressBar = show));
+    this.progressService.showOrderSuccessModal$.subscribe(
+      (show) => (this.showOrderSuccesModal = show)
+    );
+    this.progressService.cartItemCount$.subscribe((count) => (this.cartItemCount = count));
   }
 
+  closeOrderSuccessModal(): void {
+    this.progressService.closeOrderSuccessModal();
+  }
+}
